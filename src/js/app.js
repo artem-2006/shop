805c7e4f73d0afac34b91f3bd899906197a42f70
artem-2006 <ghgfameli@gmail.com>
2025-01-22 12:17:14 +0300

    01.01

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,18 @@ changeDisplay();
 AOS.init({
 	once: true,
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+	new Splide("#instagramSlider", {
+		type: "loop",
+		perPage: 4,
+		gap: "1rem",
+		autoplay: true,
+		pagination: false,
+		breakpoints: {
+			1200: { perPage: 3 },
+			768: { perPage: 2 },
+			576: { perPage: 1 },
+		},
+	}).mount();
+});
